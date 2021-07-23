f08e29dfd3698d924a8df58754a0818ab88427e5
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2021-07-23 20:58:22 +0200

    remove unused imports

--- sandwich-backend/src/main/java/com/store/sandwich/services/SandwichService.java
+++ sandwich-backend/src/main/java/com/store/sandwich/services/SandwichService.java
@@ -1,18 +1,8 @@
 package com.store.sandwich.services;
 
 import com.store.sandwich.requests.SandwichRequest;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-
-import java.util.Map;
 
 public interface SandwichService {
 
     void orderSandwich(SandwichRequest sandwichRequest);
-
-//    public abstract Double addPriceProductComposition(RequestSandwich requestSandwich, Double currentPrice);
-//
-//    public abstract Double verifyStock(Sandwich sandwich);
-
-//    Map<String, Double> mapPriceProductComposition(RequestSandwich requestSandwich);
-
 }
